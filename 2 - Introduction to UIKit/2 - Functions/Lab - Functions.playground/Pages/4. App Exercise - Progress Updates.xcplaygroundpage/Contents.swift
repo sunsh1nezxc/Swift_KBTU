e2f5025bca48abdb11dee0a543af754dbd907351
sunshine <69354535+sunsh1nezxc@users.noreply.github.com>
2024-02-19 16:50:24 +0600

    3
    3

---
+++ 2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+/*:
+## App Exercise - Progress Updates
+ 
+ >These exercises reinforce Swift concepts in the context of a fitness tracking app.
+ 
+ In many cases you want to provide input to a function. For example, the progress function you wrote in the Functioning App exercise might be located in an area of your project that doesn't have access to the value of `steps` and `goal`. In that case, whenever you called the function, you would need to provide it with the number of steps that have been taken and the goal for the day so it can print the correct progress statement.
+ 
+ Rewrite the function `progressUpdate`, only this time give it two parameters of type `Int` called `steps` and `goal`, respectively. Like before, it should print "You're off to a good start." if steps is less than 10% of goal, "You're almost halfway there!" if steps is less than half of goal, "You're over halfway there!" if steps is less than 90% of goal, "You're almost there!" if steps is less than goal, and "You beat your goal!" otherwise. Call the function and observe the printout.
+ 
+ Call the function a number of times, passing in different values of `steps` and `goal`. Observe the printouts and make sure what is printed to the console is what you would expect for the parameters passsed in.
+ */
+func progressUpdate(steps: Int, goal: Int){
+    let e = Double(steps) / Double(goal)
+    if e <= 0.1{
+        print("You're off to a good start.")
+    } else if(e <= 0.5){
+        print("You're almost halfway there!")
+    } else if(e <= 0.9){
+        print("You're almost there!")
+    } else{
+        print("You beat your goal!")
+    }
+}
+progressUpdate(steps: 20000, goal: 20000)
+
+//:  Your fitness tracking app is going to help runners stay on pace to reach their goals. Write a function called pacing that takes four `Double` parameters called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. Your function should calculate whether or not the user is on pace to hit or beat `goalTime`. If yes, print "Keep it up!", otherwise print "You've got to push it just a bit harder!"
+func pacing(currentDistance: Double, totalDistance: Double, currentTime: Double, goalTime: Double) {
+    if currentTime <= (goalTime / totalDistance) * currentDistance {
+        print("Keep it up!")
+    } else {
+        print("You've got to push it just a bit harder!")
+    }
+}
+
+pacing(currentDistance: 5, totalDistance: 10, currentTime: 20, goalTime: 40)
+
+
+/*:
+[Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Return Values](@next)
+ */
---
+++ 2 - Introduction to UIKit/3 - Structures/Lab - Structures.playground/Pages/1. Exercise - Structs, Instances, and Default Values.xcplaygroundpage/Contents.swift
@@ -0,0 +1,46 @@
+/*:
+## Exercise - Structs, Instances, and Default Values
+ 
+ Imagine you are creating an app that will monitor location. Create a `GPS` struct with two variable properties, `latitude` and `longitude`, both with default values of 0.0.
+ */
+struct GPS{
+    var latitude = 0.0
+    var longitude = 0.0
+}
+
+var o = GPS()
+
+//:  Create a variable instance of `GPS` called `somePlace`. It should be initialized without supplying any arguments. Print out the latitude and longitude of `somePlace`, which should be 0.0 for both.
+var somePlace = GPS()
+print(somePlace.latitude, somePlace.longitude)
+
+//:  Change `somePlace`'s latitude to 51.514004, and the longitude to 0.125226, then print the updated values.
+somePlace.latitude = 51.514004
+somePlace.longitude = 0.125226
+
+print(somePlace.latitude, somePlace.longitude)
+
+//:  Now imagine you are making a social app for sharing your favorite books. Create a `Book` struct with four variable properties: `title`, `author`, `pages`, and `price`. The default values for both `title` and `author` should be an empty string. `pages` should default to 0, and `price` should default to 0.0.
+struct Book {
+    var title: String = ""
+    var author: String = ""
+    var pages: Int = 0
+    var price: Double = 0.0
+}
+
+var myBook = Book()
+
+print("Title: \(myBook.title), Author: \(myBook.author), Pages: \(myBook.pages), Price: \(myBook.price)")
+
+
+
+//:  Create a variable instance of `Book` called `favoriteBook` without supplying any arguments. Print out the title of `favoriteBook`. Does it currently reflect the title of your favorite book? Probably not. Change all four properties of `favoriteBook` to reflect your favorite book. Then, using the properties of `favoriteBook`, print out facts about the book.
+myBook.title = "Так говорил Заратустра"
+myBook.author = "Ницше"
+myBook.pages = 458
+myBook.price = 850
+print("Title: \(myBook.title) \n Author: \(myBook.author) \n Pages: \(myBook.pages) \n Price: \(myBook.price)")
+
+/*:
+page 1 of 10  |  [Next: App Exercise - Workout Tracking](@next)
+ */
